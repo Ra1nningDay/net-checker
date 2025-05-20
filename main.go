3bfaf898d20d9219d8a89323ef5a6f170069be1f
Ra1nningDay <tkhamkawe@gmail.com>
2025-05-21 00:33:27 +0700

    feat: add basic host validate and err handling in ping func

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func ping(host string) (string, error) {
+	if host == "" {
+		return "", fmt.Errorf("host is empty")
+	}
+
+	return host, nil
+}
+
 func main() {
-	fmt.Printf("Hello World")
+	host := ""
+	result, err := ping(host)
+	if err != nil {
+		fmt.Printf("Error %s: %v", host, err)
+		return
+	}
+	fmt.Printf("Ping: %s: \n%s\n", host, result)
 }
